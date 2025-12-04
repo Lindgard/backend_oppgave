2418c7039986251c40a2ffa412a1fb0bff7038ef
pisani <a.b.lindgard@gmail.com>
2025-12-04 20:12:04 +0100

    Changed the parsing for the switch to allow decimals not just pure integers

--- Program.cs
+++ Program.cs
@@ -25,7 +25,7 @@ class Program
             //* The switch looped through for the correct response
             //* depending on input converted in the TryParse to 
             //* an integer called temperature. Colorcoded responses.
-            if (int.TryParse(input, out int temperatureCelsius))
+            if (double.TryParse(input, out double temperatureCelsius))
             {
                 //* Added Fahrenheit conversion for added options.
                 double temperatureFahrenheit = (temperatureCelsius * 9.0 / 5.0) + 32;
@@ -34,37 +34,37 @@ class Program
                 {
                     case <= 0:
                         Console.ForegroundColor = ConsoleColor.Blue;
-                        Console.WriteLine($"It's freezing cold outside. ({temperatureCelsius}°C / {temperatureFahrenheit:F1}°F)");
+                        Console.WriteLine($"It's freezing cold outside. ({temperatureCelsius:F1}°C / {temperatureFahrenheit:F1}°F)");
                         Console.ResetColor();
                         break;
 
                     case > 0 and <= 15:
                         Console.ForegroundColor = ConsoleColor.Cyan;
-                        Console.WriteLine($"It's pretty chilly outside.({temperatureCelsius}°C / {temperatureFahrenheit:F1}°F)");
+                        Console.WriteLine($"It's pretty chilly outside.({temperatureCelsius:F1}°C / {temperatureFahrenheit:F1}°F)");
                         Console.ResetColor();
                         break;
 
                     case > 15 and <= 25:
                         Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine($"The weather is mild and comfortable.({temperatureCelsius}°C / {temperatureFahrenheit:F1}°F)");
+                        Console.WriteLine($"The weather is mild and comfortable.({temperatureCelsius:F1}°C / {temperatureFahrenheit:F1}°F)");
                         Console.ResetColor();
                         break;
 
                     case > 25 and <= 35:
                         Console.ForegroundColor = ConsoleColor.Yellow;
-                        Console.WriteLine($"It's pretty hot, but still comfortable outside.({temperatureCelsius}°C / {temperatureFahrenheit:F1}°F)");
+                        Console.WriteLine($"It's pretty hot, but still comfortable outside.({temperatureCelsius:F1}°C / {temperatureFahrenheit:F1}°F)");
                         Console.ResetColor();
                         break;
 
                     case > 35 and <= 45:
                         Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine($"It's very very hot outside.({temperatureCelsius}°C / {temperatureFahrenheit:F1}°F)");
+                        Console.WriteLine($"It's very very hot outside.({temperatureCelsius:F1}°C / {temperatureFahrenheit:F1}°F)");
                         Console.ResetColor();
                         break;
 
                     case > 45:
                         Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine($"It's dangerous to be outside too long.({temperatureCelsius}°C / {temperatureFahrenheit:F1}°F)");
+                        Console.WriteLine($"It's dangerous to be outside too long.({temperatureCelsius:F1}°C / {temperatureFahrenheit:F1}°F)");
                         Console.ResetColor();
                         break;
                 }
