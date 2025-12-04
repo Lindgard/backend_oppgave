90203713139ed3762e2698a4f25bb918eed1900a
pisani <a.b.lindgard@gmail.com>
2025-12-04 18:12:22 +0100

    wrote the switch cases needed and added an else to the if-statement to warn of invalid input

--- Program.cs
+++ Program.cs
@@ -8,17 +8,29 @@ class Program
 
         string? input = Console.ReadLine();
 
-        if(int.TryParse(input, out int temperature))
+        if (int.TryParse(input, out int temperature))
         {
             switch (temperature)
             {
                 case <= 0:
-                Console.WriteLine("It's freezing cold outside.");
-                break;
+                    Console.WriteLine("It's freezing cold outside.");
+                    break;
 
                 case > 0 and <= 15:
-                Console.WriteLine("It's pretty chilly outside.");
-                break;
+                    Console.WriteLine("It's pretty chilly outside.");
+                    break;
+
+                case > 15 and <= 25:
+                    Console.WriteLine("The weather is mild and comfortable.");
+                    break;
+
+                case > 25 and <= 35:
+                    Console.WriteLine("It's pretty hot outside.");
+                    break;
+
+                case > 35:
+                    Console.WriteLine("It's stupid hot outside.");
+                    break;
             }
         }
     }
