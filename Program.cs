e849910f7d15ed48e7f0710b79e8fe8208d78c0f
pisani <a.b.lindgard@gmail.com>
2025-12-04 17:55:53 +0100

    Added the welcome-message requesting an input, a read for the input and started the switch

--- Program.cs
+++ Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Enter a temperature in Celsius:");
+
+        string? input = Console.ReadLine();
+
+        if(int.TryParse(input, out int temperature))
+        {
+            switch (temperature)
+            {
+                case <= 0:
+                Console.WriteLine("It's freezing cold outside.");
+                break;
+
+                case > 0 and <= 15:
+                Console.WriteLine("It's pretty chilly outside.");
+                break;
+            }
+        }
     }
 }
